02cc2aab90a37a46abc400c4f8c495be6c37c9a5
alemespinozav <alem.espinoza@profesor.duoc.cl>
2026-06-19 19:33:49 -0400

    este es mi examen xao

--- producto-service/src/main/java/cl/duoc/producto_service/controller/ProductoController.java
+++ producto-service/src/main/java/cl/duoc/producto_service/controller/ProductoController.java
@@ -1,19 +1,23 @@
 package cl.duoc.producto_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/")
 public class ProductoController {
 
-    @GetMapping("/api/productos")
+
+
+    @GetMapping("/productos")
     public List<String> listarProductos() {
         return List.of("Notebook", "Mouse", "Teclado");
     }
 
-    @GetMapping("/api/productos/estado")
+    @GetMapping("/productos/estado")
     public String estado() {
         return "Producto Service funcionando correctamente en el puerto 8081";
     }
